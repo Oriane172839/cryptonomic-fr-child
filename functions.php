86697bfb42b62924d29aba2d04ad8fa9a1c45865
Oriane172839 <nyikeineoriane@gmail.com>
2026-01-21 18:40:09 +0900

    Fix: force parent + blogvi styles loading

--- functions.php
+++ functions.php
@@ -1,14 +1,25 @@
 <?php
-add_action('wp_enqueue_scripts', function() {
-  wp_enqueue_style(
-    'parent-style',
-    get_template_directory_uri() . '/style.css'
-  );
+add_action( 'wp_enqueue_scripts', function() {
 
-  wp_enqueue_style(
-    'child-style',
-    get_stylesheet_uri(),
-    ['parent-style'],
-    wp_get_theme()->get('Version')
-  );
+    // CSS du parent réel
+    wp_enqueue_style(
+        'bloglo-style',
+        get_template_directory_uri() . '/style.css'
+    );
+
+    // CSS du thème actuel remplacé (Blogvi)
+    wp_enqueue_style(
+        'blogvi-style',
+        get_stylesheet_directory_uri() . '/../blogvi/style.css',
+        ['bloglo-style']
+    );
+
+    // CSS du thème enfant
+    wp_enqueue_style(
+        'child-style',
+        get_stylesheet_uri(),
+        ['blogvi-style'],
+        wp_get_theme()->get('Version')
+    );
+
 });
