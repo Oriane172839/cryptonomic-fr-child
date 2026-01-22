e7030f469d8488e434de095d996d560e74f4bd07
Oriane172839 <nyikeineoriane@gmail.com>
2026-01-22 12:00:59 +0900

    Force favicon via wp_head for Google SERP

--- functions.php
+++ functions.php
@@ -1,13 +1,23 @@
 <?php
+
+// Charger les styles parent + enfant
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style(
         'parent-style',
         get_template_directory_uri() . '/style.css'
     );
+
     wp_enqueue_style(
         'child-style',
         get_stylesheet_uri(),
         ['parent-style'],
         wp_get_theme()->get('Version')
     );
+});
+
+// Forcer le favicon pour Google (SERP)
+add_action('wp_head', function () {
+    ?>
+    <link rel="icon" href="https://www.cryptonomic.fr/wp-content/uploads/2026/01/favicon.png" sizes="512x512" type="image/png">
+    <?php
 });
